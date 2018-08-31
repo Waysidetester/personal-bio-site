fac4e2e5baae071b9432ccd21aea770801ae4564
WaysideTester <dylanmurray2119@gmail.com>
2018-08-30 20:43:47 -0500

    starting styling

--- projects.js
+++ projects.js
@@ -5,13 +5,13 @@ const projects = [
         alt: "Cat",
         description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
         technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-        available: false,
+        available: true,
         url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
         githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
     },
     {
         title: "Cool Project", 
-        screenshot: "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?cs=srgb&dl=beach-exotic-holiday-248797.jpg&fm=jpg", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
         alt: "Cat again",
         description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
         technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
@@ -30,10 +30,10 @@ const createProjectCards = () => {
     let newCard = '';
     for (i = 0; i < projects.length; i++) {
         if (projects[i].available) {
-            newCard = '';
+            newCard = ''; // empties newCard for new text
             newCard += `<section class="projectCard">`;
             newCard +=      `<h4 class="projectTitle">${projects[i].title}</h4>`;
-            newCard +=      `<a href="${projects[i].url}"><img src="${projects[i].screenshot}" alt="${projects[i].alt}"/></a>`;
+            newCard +=      `<a class="screenShot" href="${projects[i].url}"><img src="${projects[i].screenshot}" alt="${projects[i].alt}"/></a>`;
             newCard +=      `<p class="projectDescription">${projects[i].description}</p>`;
             newCard +=      `<p class="projectTechUsed">${projects[i].technologiesUsed}</p>`;
             newCard +=      `<a href="${projects[i].githubUrl}">Github Repository</a>`;
